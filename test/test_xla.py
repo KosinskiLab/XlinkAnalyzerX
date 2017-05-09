6c24b920b80e255f58f0e17b49def0033787c35f
Jan Kosinski <kosinski@embl.de>
2017-05-09 08:52:23 +0200

    unittesting gui

--- test/test_xla.py
+++ test/test_xla.py
@@ -1,9 +1,11 @@
 import unittest
 import os
-
+import sys
+# for k in sys.modules.keys():
+#     print(k)
 import chimerax.xlinkanalyzer
 
-class test_window(unittest.TestCase):
+class test_xla(unittest.TestCase):
     '''
     example test
     '''
@@ -12,6 +14,12 @@ class test_window(unittest.TestCase):
 
     def test_gui(self):
         self.assertEqual(4, 4)
+        print(dir(session.ui))
+        print(session.ui.main_window)
 
-if __name__ == '__main__':
+
+print(__name__)
+sys.modules['__main__'].test_xla = test_xla
+# if __name__ == '__main__':
+if __name__ == 'ChimeraX_sandbox_1':
     unittest.main()
